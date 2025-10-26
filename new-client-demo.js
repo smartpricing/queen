5bcc02f465414f11e36c73656049e23c9d75d59c
Alice <alice.viola.setti@gmail.com>
2025-10-26 07:10:41 +0100

    before registry

--- new-client-demo.js
+++ new-client-demo.js
@@ -1,41 +1,14 @@
-import { Queen } from 'queen-mq'
+import { queen } from 'queen-mq'
 
-const client = new Queen({ 
-    baseUrls: ['http://localhost:6632'],
-    timeout: 30000,
-    retryAttempts: 3
-});
-
-const queue = 'html-processing'
-
-// Create a queue
-await client.queue(queue, { leaseTime: 30 });
-
-// Push some data, specifyng the partition
-await client.tx(`${queue}/customer-1828`, [ { id: 1 } ]); 
-
-await client.rx(queue, { limit: 1, batch: 10, wait: true, timeout: 30000 }, async (msg) => {
-    console.log(msg.data.id)
-});
-
-// Consume data from any partition of the queue, continusly
-// This "pipeline" is useful for doing exactly one processing
-await client 
-.pipeline(queue)
-.withAutoRenewal({ 
-  interval: 5000  // Renew lease every 5 seconds
-})    
-.withConcurrency(5) // Five parallel promises
-.rx(10, {
-  wait: true, // Use long polling
-  timeout: 30000 // Long polling length in millisconds
+queen.queue('queue', { leaseTime: 30 })
+.tx('queue/partition0', [ { id: 1 }, { id: 2 }, { id: 3 } ])
+.rx('queue', { limit: 1, batch: 10, wait: true, timeout: 30000, autoRenewalInterval: 5000, workers: 2 })
+.process(async (msgs) => {
+  console.log(msgs.map(x => x.data.id))
+  return msgs.map(x => x.data.id * 2)
 })
-.processBatch(async (messages) => {
-    return messages.map(x => x.data.id * 2);
+.transaction(async (trx, msgs) => {
+  trx.ack(msgs)
+  trx.tx('queue-another/partition0', msgs.map(x => x.data.id * 2))
 })
-.transaction((tx, originalMessages, processedMessages) => { // ack and push are transactional inside atomically
-    tx.ack(originalMessages);
-    tx.tx('another-queue', processedMessages); 
-})
-.repeat({ continuous: true })
-.execute();
+.execute()
